462d01e77f067a6702c40cf9016c61587aab701e
TejasBadhe07 <tbadhe75@gmail.com>
2024-05-09 11:37:20 +0530

    components update in Quiz

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='Registration-Form/templates')
 
 @app.route('/')
 def index():
-    return render_template('Registration-Form/templates/index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
